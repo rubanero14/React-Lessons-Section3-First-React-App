2595f70f616b98421f9afd84f5f12daa38b52c1d
Rajaruban Rajindram <ahyat888@gmail.com>
2022-09-15 20:35:43 +0800

    added first child component for App.js

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,5 +1,13 @@
-function ExpenseItem(){
-    return <h4>Expense Item Page</h4>;
+function ExpenseItem() {
+  return (
+    <div>
+      <h2>15th September 2022</h2>
+      <div>
+        <div>Car Insurance</div>
+        <div>RM 490.00</div>
+      </div>
+    </div>
+  );
 }
 
 export default ExpenseItem;
