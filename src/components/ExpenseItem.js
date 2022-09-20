5bd55bcd9d925abba6c7df182f3b9959eb71cc6f
Rajaruban Rajindram <ahyat888@gmail.com>
2022-09-20 16:39:58 +0800

    added basic css stling into ExpenseItem.js component

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,10 +1,12 @@
+import "./ExpenseItem.css";
+
 function ExpenseItem() {
   return (
-    <div>
+    <div className="expense-item">
       <h2>15th September 2022</h2>
-      <div>
+      <div className="expense-item__description">
         <div>Car Insurance</div>
-        <div>RM 490.00</div>
+        <div className="expense-item__price">RM 490.00</div>
       </div>
     </div>
   );
