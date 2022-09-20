d577a14b94b31e69ce13d12fd233f4b485647fcc
Rajaruban Rajindram <ahyat888@gmail.com>
2022-09-20 17:11:15 +0800

    Outputting Dynamic Data & Working with Expressions in JS

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,12 +1,18 @@
 import "./ExpenseItem.css";
 
+// Component Logic Section
+const expenseDate = new Date(2022, 8, 20);
+const expenseTitle = "Car Insurance";
+const expenseAmount = 294.67;
+
+// Component Template Section
 function ExpenseItem() {
   return (
     <div className="expense-item">
-      <h2>15th September 2022</h2>
+      <h2>{expenseDate.toISOString()}</h2>
       <div className="expense-item__description">
-        <div>Car Insurance</div>
-        <div className="expense-item__price">RM 490.00</div>
+        <div>{expenseTitle}</div>
+        <div className="expense-item__price">RM{expenseAmount}</div>
       </div>
     </div>
   );
