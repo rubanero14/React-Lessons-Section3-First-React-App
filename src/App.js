ed57f38f688137ccb01f46b93d3a08ed7fb58e94
Rajaruban Rajindram <ahyat888@gmail.com>
2022-09-25 00:10:15 +0800

    Passing Data via props to components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,59 @@
 import ExpenseItem from "./components/ExpenseItem";
 
 function App() {
+  const Expenses = [
+    {
+      id: 1,
+      title: "Car Insurance",
+      amount: 483.0,
+      date: new Date(2022, 8, 20),
+    },
+    {
+      id: 2,
+      title: "Mortgage",
+      amount: 2500.0,
+      date: new Date(2022, 8, 21),
+    },
+    {
+      id: 3,
+      title: "Car Service",
+      amount: 1300.0,
+      date: new Date(2022, 8, 2),
+    },
+    {
+      id: 4,
+      title: "New Udemy Course",
+      amount: 70.0,
+      date: new Date(2022, 8, 30),
+    },
+  ];
+
   return (
     <div>
       <h2>Let's get started!</h2>
       <p>This is also visible!</p>
-      <ExpenseItem />
+      <ExpenseItem
+        title={Expenses[0].title}
+        amount={Expenses[0].amount}
+        date={Expenses[0].date}
+      />
+      <ExpenseItem
+        title={Expenses[1].title}
+        amount={Expenses[1].amount}
+        date={Expenses[1].date}
+      />
+      `
+      <ExpenseItem
+        title={Expenses[2].title}
+        amount={Expenses[2].amount}
+        date={Expenses[2].date}
+      />
+      <ExpenseItem
+        title={Expenses[3].title}
+        amount={Expenses[3].amount}
+        date={Expenses[3].date}
+      />
+      `
     </div>
   );
 }
--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,18 +1,13 @@
 import "./ExpenseItem.css";
 
-// Component Logic Section
-const expenseDate = new Date(2022, 8, 20);
-const expenseTitle = "Car Insurance";
-const expenseAmount = 294.67;
-
 // Component Template Section
-function ExpenseItem() {
+function ExpenseItem(props) {
   return (
     <div className="expense-item">
-      <h2>{expenseDate.toISOString()}</h2>
+      <h2>{props.date.toISOString()}</h2>
       <div className="expense-item__description">
-        <div>{expenseTitle}</div>
-        <div className="expense-item__price">RM{expenseAmount}</div>
+        <div>{props.title}</div>
+        <div className="expense-item__price">RM{props.amount}</div>
       </div>
     </div>
   );
