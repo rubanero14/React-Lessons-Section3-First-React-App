c92ecc403a27134dc7a4decc1427715a7a4eb5fb
Rajaruban Rajindram <ahyat888@gmail.com>
2022-10-20 18:12:36 +0800

    Working with State in React

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -2,19 +2,25 @@
 import "./ExpenseItem.css";
 
 // Import child component for this component
+import React, { useState } from "react";
 import ExpenseDate from "./ExpenseDate";
 import Card from "../UI/Card";
 
 // Component Template Section
-const clickHandler = () => {
-  console.log("Clicked");
-};
 const ExpenseItem = (props) => {
+  // useState hook is used to dynamically change values of props or data on the DOM realtime, without it, the page is static
+  // useState() takes a param, which is variable that will be changed or manipulated, and useState(var) returns an array with 2 values [var, varManipulatorFunction]
+  const [title, setTitle] = useState(props.title);
+  const clickHandler = () => {
+    // setTitle() is a varManipulatorFunction that takes a new param which is the new value to be assigned replacing the old one and same time refreshes the DOM
+    setTitle(`${props.title} ${(Math.random(0, 1) * 10).toFixed(0)}`);
+  };
+
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date} />
       <div className="expense-item__description">
-        <h2>{props.title}</h2>
+        <h2>{title}</h2>
         <div className="expense-item__price">RM{props.amount}</div>
       </div>
       <button onClick={clickHandler}>Change Title</button>
