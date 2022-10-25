80fcf29dbed5c2dcbaca99ba4d557b4a9231faac
Rajaruban Rajindram <ahyat888@gmail.com>
2022-10-25 11:37:18 +0800

    Working with Multiple States

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -2,9 +2,20 @@ import "./ExpenseForm.css";
 import React, { useState } from "react";
 
 const ExpenseForm = (props) => {
-  let titleValue;
-  const titleChangeHandler = (event) => {
-    console.log(event.target.value);
+  // storing input value into reactive useState()
+  const [enteredTitle, setEnteredTitle] = useState("");
+  const [enteredAmount, setEnteredAmount] = useState("");
+  const [enteredDate, setEnteredDate] = useState("");
+
+  const titleChangeHandler = (e) => {
+    // storing the new value into enteredTitle
+    setEnteredTitle(e.target.value);
+  };
+  const amountChangeHandler = (e) => {
+    setEnteredAmount(e.target.value);
+  };
+  const dateChangeHandler = (e) => {
+    setEnteredDate(e.target.value);
   };
 
   return (
@@ -12,15 +23,25 @@ const ExpenseForm = (props) => {
       <div className="new-expense__controls">
         <div className="new-expense__control">
           <label>Title</label>
-          <input type="text" onChange={titleChangeHandler} value={titleValue} />
+          <input type="text" onChange={titleChangeHandler} />
         </div>
         <div className="new-expense__control">
-          <label>Amunt</label>
-          <input type="number" min="0.01" step="0.01" />
+          <label>Amount</label>
+          <input
+            type="number"
+            min="0.01"
+            step="0.01"
+            onChange={amountChangeHandler}
+          />
         </div>
         <div className="new-expense__control">
           <label>Date</label>
-          <input type="date" min="2021-01-01" max="2023-12-31" />
+          <input
+            type="date"
+            min="2021-01-01"
+            max="2023-12-31"
+            onChange={dateChangeHandler}
+          />
         </div>
         <div className="new-actions">
           <button type="submit">Add Expense</button>
