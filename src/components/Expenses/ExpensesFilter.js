97929997307e6fb1afd28e3bd9812e2fe54ed5de
Rajaruban Rajindram <ahyat888@gmail.com>
2022-10-30 13:46:02 +0800

    added dynamiccally generated values and removed hardcoded ones in the select filter markup

--- src/components/Expenses/ExpensesFilter.js
+++ src/components/Expenses/ExpensesFilter.js
@@ -6,15 +6,19 @@ const ExpensesFilter = (props) => {
     props.onFilterDateChange(e.target.value);
   };
 
+  const setYearValues = (n) => {
+    return (new Date().getFullYear() + 1 - n).toString();
+  };
   return (
     <div className="expenses-filter">
       <div className="expenses-filter__control">
         <label>Filter by year</label>
         <select onChange={dateFilterHandler} value={props.initialYear}>
-          <option value="2022">2022</option>
-          <option value="2021">2021</option>
-          <option value="2020">2020</option>
-          <option value="2019">2019</option>
+          <option value={setYearValues(0)}>{setYearValues(0)}</option>
+          <option value={setYearValues(1)}>{setYearValues(1)}</option>
+          <option value={setYearValues(2)}>{setYearValues(2)}</option>
+          <option value={setYearValues(3)}>{setYearValues(3)}</option>
+          <option value={setYearValues(4)}>{setYearValues(4)}</option>
         </select>
       </div>
     </div>
